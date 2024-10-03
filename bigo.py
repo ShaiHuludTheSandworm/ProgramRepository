3a592079eb3ec7d6603ac2e29c8c0de24f804a7f
ShaiHuludTheSandworm <145036835+ShaiHuludTheSandworm@users.noreply.github.com>
2024-10-02 19:38:32 -0500

    added pseudocode

--- bigo.py
+++ bigo.py
@@ -45,6 +45,15 @@ def length_of_longest_substring_n2(s):
 # TODO: implement this function. You may delete this comment when you are done.
 def length_of_longest_substring_n(s):
     """
+      letters are added to a set, if the set does not increase when added to then it has found a duplicate
+      a separate variable keeps track of set size before new letter is added
+      if letter is added to set, but the set size does not increase it goes back by the length of the set and checks
+      that letter against the current location in the for loop, if that letter is the duplicate then the length
+      of the substring goes down by 1, if that letter is not the duplicate that letter is removed from the set
+      and the substring length goes down by 1, once the duplicate is reached and the substring went down by 1
+      the set does not have that letter removed since we are already at it again
+    """
+    """
     Finds the length of the longest substring without repeating characters
     using a frequency list approach (O(N)), converting each character to
     their corresponding numeric representation in ASCII as the index into the
