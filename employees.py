a280301f4d3e96ed00180c47f03bf3fa26044793
ShaiHuludTheSandworm <145036835+ShaiHuludTheSandworm@users.noreply.github.com>
2024-10-13 23:11:01 -0500

    nvm then

--- employees.py
+++ employees.py
@@ -101,7 +101,7 @@ class Manager(Employee):
     # manager: shows none because managers dont have a manager
     @property
     def manager(self):
-        return self.__Employee__manager
+        return None
 
     # performance: sets up performance veriable to be accessed with the setter method
     @property
